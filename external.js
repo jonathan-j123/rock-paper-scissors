a80ca100d78cbf92a042b1ce150986867de34aa6
nameless-bastard <wewlococoz@gmail.com>
2022-06-22 18:40:22 +0800

    FIX: upload working javascript

--- external.js
+++ external.js
@@ -1,11 +1,54 @@
+const count = {
+    userScore: 0,
+    computerScore: 0,
+}
+
+function game() {
+    for (count.userScore = 0, count.computerScore; count.userScore < 5 || count.computerScore < 5;) {
+       setTimeout( gameExecution( computerPlay(), prompt(`User: ${count.userScore} | Computer: ${count.computerScore}\n['rock', 'paper', 'scissor'] - Choose one: `).toString()),
+       1000
+       );
+     }
+
+}
+
 function computerPlay() {
-    return Math.floor(Math.random() * 3 + 1);
+    const randomAnswer = Math.floor(Math.random() * 3 + 1);
+    return (randomAnswer === 1)? "rock":
+    (randomAnswer === 2)? "paper": "scissor";
 }
 
 function gameExecution(computerSelection, userSelection) {
-    computerSelection = computerPlay();
-    let userInput = prompt("['ROCK', 'PAPER', 'SCISSOR'] - Choose one: ");
-    return
+    userSelection = userSelection.toLowerCase();
+
+    //     computerPlay() =  Rock:1 Paper:2   Scissor:3  so this will be the guide on how to decide if identical or not
+    const winMessage = `you win! ${userSelection} beats ${computerSelection}.`;
+    const loseMessage = `you lose! ${userSelection} beats by ${computerSelection}.`;
+    const drawMessage = `DRAW! your ${userSelection} is same with computer's ${userSelection}.`;
+
+    // ----------------WIN---------------------------
+    const result = (userSelection === "rock" && computerSelection === "scissor")? `User Points: ${++count.userScore}, ${winMessage}`:
+    (userSelection === "scissor" && computerSelection === "paper")? `User Points: ${++count.userScore}, ${winMessage}`:
+    (userSelection === "paper" && computerSelection === "rock")? `User Points: ${++count.userScore}, ${winMessage}`:
+    // ---------------LOSE---------------------------
+    (userSelection === "paper" && computerSelection === "scissor")? `Computer Points: ${++count.computerScore }, ${loseMessage}`:
+    (userSelection === "scissor" && computerSelection ==="rock")? `Computer Points: ${++count.computerScore}, ${loseMessage}`: 
+    (userSelection === "rock" && computerSelection === "paper")? `Computer Points: ${++count.computerScore}, ${loseMessage}`:
+    // --------------DRAW---------------------------
+    (userSelection === 'scissor' && computerSelection === "scissor")? `${drawMessage}`:
+    (userSelection === 'paper' && computerSelection === "paper")? `${drawMessage}`:
+    (userSelection === 'rock' && computerSelection === "rock")? `${drawMessage}`:
+    //---------------ERROR--------------------------
+    (userSelection !== "rock" || userSelection !== "paper" || userSelection !== "scissor")? userSelectionWrong(): "Something Happen, Error";
+
+    alert(result);
 }
 
+function userSelectionWrong() {
+    // Incase of user inputs not in choices, then this will trigger
+    alert("Your input is not in the choices. Please follow the program. Repeating Process..");
+    gameExecution();
+}
+
+game();
 
