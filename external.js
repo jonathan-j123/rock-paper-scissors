7f6b336094c1aec6cfd8dad20be9f95fd72b8586
nameless-bastard <wewlococoz@gmail.com>
2022-06-22 15:04:37 +0800

    Update: making function and moving new var

--- external.js
+++ external.js
@@ -1,5 +1,11 @@
-let userInput = prompt("['ROCK', 'PAPER', 'SCISSOR'] - Choose one: ");
 function computerPlay() {
     return Math.floor(Math.random() * 3 + 1);
 }
+
+function gameExecution(computerSelection, userSelection) {
+    computerSelection = computerPlay();
+    let userInput = prompt("['ROCK', 'PAPER', 'SCISSOR'] - Choose one: ");
+    return
+}
+
 
