9d5d471ff828d1d94d5acdcab2950b7b7bf34904
nameless-bastard <wewlococoz@gmail.com>
2022-06-25 09:32:13 +0800

    Terminate function, ternary is bad, but I just continued.

--- external.js
+++ external.js
@@ -3,40 +3,42 @@ const count = {
     computerScore: 0,
 }
 
-function game() {
-    for (; count.userScore < 5 && count.computerScore < 5;) {
-       gameExecution( computerPlay(), prompt(`User: ${count.userScore} | Computer: ${count.computerScore}\n['rock', 'paper', 'scissor'] - Choose one: `).toString());
-
-       
-     }
-     let winner = (count.userScore >= 5)? "Cool Player win! " : "Mighty Computer win! ";
-
-     let playAgain = confirm(`${winner}Would you like to try again?`);
-     if(playAgain){
-        count.userScore = 0; 
-        count.computerScore = 0; 
-        game(); 
-        
-     }else{
-        alert("Thanks for Playing! If you change your mind, just reload the game.");
-    } 
-
-}
-
 function computerPlay() {
     const randomAnswer = Math.floor(Math.random() * 3 + 1);
     return (randomAnswer === 1)? "rock":
     (randomAnswer === 2)? "paper": "scissor";
 }
 
-function gameExecution(computerSelection, userSelection) {
-    userSelection = userSelection.toLowerCase();
+function game() {
+    for (; count.userScore < 5 && count.computerScore < 5 && count.userScore !== null;) {
+       gameExecution( computerPlay(), prompt(`User: ${count.userScore} | Computer: ${count.computerScore}\n['rock', 'paper', 'scissor'] - CHOOSE ONE: `));  
+    }
+    let result = (count.userScore >= 5)? "Cool Player win! " : 
+    (count.computerScore >= 5)? "Mighty Computer win! " :  
+    (count.userScore === null)?"You just quit the game, ": "Something is wrong with the code";
+    let playAgain = confirm(`${result}Would you like to try again?\n[Cancel] - No\n[Okay] - Yes *Scores = 0*`);
+    if(playAgain){
+        count.userScore = 0; 
+        count.computerScore = 0;
+        count.terminate = false;
+        game(); 
+        
+    }else{
+        alert("Thanks for Playing! If you change your mind, just reload the game.");
+    } 
+}
 
+function gameExecution(computerSelection, userSelection) {
+
+    while (userSelection !== "rock" && userSelection !== "paper" && userSelection !== "scissor" && userSelection !== null) {
+        alert(`Input ${userSelection} is not at choice, please try again.`);
+        userSelection =  prompt(`User: ${count.userScore} | Computer: ${count.computerScore}\n['rock', 'paper', 'scissor'] - Choose one: `);
+        
+    }
     //     computerPlay() =  Rock:1 Paper:2   Scissor:3  so this will be the guide on how to decide if identical or not
     const winMessage = `you win! ${userSelection} beats ${computerSelection}.`;
     const loseMessage = `you lose! ${userSelection} beats by ${computerSelection}.`;
     const drawMessage = `DRAW! your ${userSelection} is same with computer's ${userSelection}.`;
-
     // ----------------WIN---------------------------
     const result = (userSelection === "rock" && computerSelection === "scissor")? `User Points: ${++count.userScore}, ${winMessage}`:
     (userSelection === "scissor" && computerSelection === "paper")? `User Points: ${++count.userScore}, ${winMessage}`:
@@ -48,19 +50,16 @@ function gameExecution(computerSelection, userSelection) {
     // --------------DRAW---------------------------
     (userSelection === 'scissor' && computerSelection === "scissor")? `${drawMessage}`:
     (userSelection === 'paper' && computerSelection === "paper")? `${drawMessage}`:
-    (userSelection === 'rock' && computerSelection === "rock")? `${drawMessage}`:
-    //---------------ERROR--------------------------
-    (userSelection !== "rock" || userSelection !== "paper" || userSelection !== "scissor")? userSelectionWrong(): 
-    (userSelection === "")? userSelectionWrong() : "Something Happen, Error";
+    (userSelection === 'rock' && computerSelection === "rock")? `${drawMessage}`: "What are the coder doing? there's wrong in the code";
+    //---------------Error/Reset-----------------------------
+    if(userSelection === null){
+        alert`Quiting the game..`;
+        return count.userScore= null;;
+    }
     
     alert(result);
 }
 
-function userSelectionWrong() {
-    // Incase of user inputs not in choices, then this will trigger
-    alert("Your input is not in the choices. Please follow the program. Repeating Process..");
-    gameExecution(computerPlay(), prompt(`User: ${count.userScore} | Computer: ${count.computerScore}\n['rock', 'paper', 'scissor'] - Choose one: `).toString());
-}
-
 game();
+
 
